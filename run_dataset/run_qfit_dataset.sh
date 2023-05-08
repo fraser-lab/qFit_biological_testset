13e35149914f0dec126c67a23de853cae5eac0ce
Stephanie (Mullane) Wankowicz <stephanie.wankowicz@ucsf.edu>
2023-05-08 10:58:50 -0700

    cleaning up
    removing wynton specific things

--- run_dataset/run_qfit_dataset.sh
+++ run_dataset/run_qfit_dataset.sh
@@ -1,22 +1,39 @@
+#!/bin/bash
+#$ -l h_vmem=4G
+#$ -l mem_free=4G
+#$ -t 1-144
+#$ -l h_rt=24:00:00
+#$ -R yes
+#$ -pe smp 8
+
+#________________________________________________INPUTS________________________________________________#
+PDB_file=/wynton/group/fraser/swankowicz/qfit_summit/expanded_dataset/list_of_pdbs_final.txt
+base_dir='/wynton/group/fraser/swankowicz/qfit_summit/expanded_dataset'
+category='test'
+export MKL_NUM_THREADS=1
+export NUMEXPR_NUM_THREADS=1
+export OMP_NUM_THREADS=1
+
+
 #________________________________________________SET PATHS________________________________________________#
-source /wynton/group/fraser/swankowicz/phenix-installer-1.20.1-4487-intel-linux-2.6-x86_64-centos6/phenix-1.20.1-4487/phenix_env.sh
-export PATH="/wynton/home/fraserlab/swankowicz/anaconda3/bin:$PATH"
+#source phenix
+source phenix_env.sh
+
+#activate qfit
 source activate qfit
-#which python
 export PHENIX_OVERWRITE_ALL=true
 
 #________________________________________________RUN QFIT________________________________________________#
- PDB_dir=$(cat $PDB_file | head -n $SGE_TASK_ID | tail -n 1)
- echo ${PDB_dir}
- cd ${base_dir}/${PDB_dir}
- pwd
- PDB=$(echo ${PDB_dir:0:4})
+ PDB=$(cat $PDB_file | head -n $SGE_TASK_ID | tail -n 1)
  echo ${PDB}
+ cd ${base_dir}/${PDB_dir}
+ 
  mkdir ${category}
  cd ${category}
- pwd
- #cp ../${PDB}.mtz .
- #cp ../composite_omit_map.mtz .
- #cp ../${PDB}.pdb .
- #qfit_protein composite_omit_map.mtz -l 2FOFCWT,PH2FOFCWT ${PDB}.pdb -p 8
+ 
+ cp ../${PDB}.mtz .
+ cp ../composite_omit_map.mtz .
+ cp ../${PDB}.updated_refine_001.pdb .
+ 
+ qfit_protein composite_omit_map.mtz -l 2FOFCWT,PH2FOFCWT ${PDB}.updated_refine_001.pdb -p 8
  qfit_final_refine_xray.sh ${PDB}.mtz multiconformer_model2.pdb
